6b74676167dc81b7cd4f8b1ca4dd2ffde69f66a8
Aksel Kaastrup Rasmussen <akara@dtu.dk>
2023-04-24 15:44:02 +0200

    ready to make data

--- Driverscripts/datamaker.m
+++ Driverscripts/datamaker.m
@@ -3,7 +3,22 @@ clc; clear; close all;
 addpath(genpath(pwd));
 %%
 priortype = 'star';
-dataSave(noiseseed, noiselevel, priortype)
-funcCall = 'matlab -nodisplay -r ''cd(''FEM/auxfun''); functionname(argument1, argument2, argumentN), [0,1]);exit';
-cmd = 'matlab -nodisplay -r curve_project';
-submitToDTUCluster('1',cmd)
+noiseseed = [1,2,3,4,5];
+noiselevel = [0.005,0.01,0.02,0.04];
+
+
+for i=1:length(noiseseed)
+    for j=1:length(noiselevele)
+        seedstr = sprintf('%0.5g',noiseseed);
+        noisestr = sprintf('%0.5g',noiselevel);
+        priorstr = sprintf('''%s''',priortype);
+        
+        matlab_call = 'matlab -nodisplay -r ''cd(''Data/dataSave'');';
+        func_call = ['dataSave(',seedstr,',',noisestr,',',priortype,')'];
+        end_call = ';exit''';
+        
+        cmd = [matlab_call, func_call, end_call];
+        
+        submitToDTUCluster([num2str(i),num2str(j)],cmd)
+    end
+end
